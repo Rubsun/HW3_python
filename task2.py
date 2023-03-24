d8474dc4e8fb7867072cdacaa3cbc20818a0d4aa
Maksim <74344870+Rubsun@users.noreply.github.com>
2023-03-24 13:32:29 +0300

    befoe check test

---
+++ task2.py
@@ -0,0 +1,14 @@
+mat = [0]
+lenm = 1
+num = int(input())
+
+
+while len(mat) < num:
+    for i in range(len(mat)):
+        if mat[i] == 0:
+            mat.append(1)
+        else:
+            mat.append(0)
+print(mat)
+print(mat[:num])
+
